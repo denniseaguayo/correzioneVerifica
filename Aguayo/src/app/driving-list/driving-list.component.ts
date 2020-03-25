5b50367a8e140a7863b0440ffbcc2bb5e7e54e8e
denniseaguayo <aguayo.dennise@iisgalvanimi.edu.it>
2020-03-25 09:39:40 +0000

    punto2

---
+++ Aguayo/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Driving } from '../driving.model';
+import {Observable} from 'rxjs';
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent implements OnInit {
+  obs: Observable<Driving[]>;
+  drivingList: Driving[];
+  constructor(public http: HttpClient) { }
+  //inizializza il componente
+  ngOnInit(): void {
+   this.obs= this.http.get<Driving[]>('https://my-json-server.typicode.com/malizia-g/fine_anno_exp/mezzi')
+   this.obs.subscribe(this.get)
+  }
+  get=(auto:Driving[]) =>
+  {
+    this.drivingList=auto;
+  }
+}
